7f0d06eacb6633e651644dbd50e5ca26f6268e79
why78901 <870111428@qq.com>
2020-01-19 23:48:59 +0800

    + <Array>  283.移动数组中的零到末位

---
+++ Assets/Scripts/MoveZeroes.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//283.移动数组中的零到末位
+public class MoveZeroes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void moveZeroes(int[] nums) {
+        if(nums.Length == 0)
+            return;
+        int moved = 0;
+        int len = nums.Length;
+        for(int i = 0; i < len; i++){
+            if(nums[i] == 0){
+                moved++;
+            }else{
+                if(moved > 0){
+                    nums[i-moved] = nums[i];
+                }
+            }
+        }
+        for(int i = len - moved - 1; i < len; i++){
+            nums[i] = 0;
+        }
+    }
+
+	public void moveZeroes2(int[] nums)
+	{
+		int slow = 0;
+		
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (nums[i] != 0)
+			{
+				nums[slow++] = nums[i];
+			}
+		}
+
+		for (int i = slow; i < nums.Length; i++)
+		{
+			nums[i] = 0;
+		}
+
+	}
+}
